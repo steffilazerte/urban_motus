eff1946b02385bdea15a864013209a6f597c9fa8
Steffi LaZerte <steffi@steffi.ca>
2024-02-14 18:19:13 -0600

    Housekeeping

--- XX_functions.R
+++ XX_functions.R
@@ -12,18 +12,44 @@ remove_by_ID <- function(src, t, id_type = "batchID", ids) {
 }
 
 
-filter_count <- function(dbs) {
+filter_count <- function(dbs, filter) {
   before <- map(dbs, \(x) tbl(x, "runs") |> count(name = "n_before") |> collect()) |>
     list_rbind(names_to = "proj_id")
-  after <- map(dbs, \(x) {
-    remove <- getRunsFilters(x, "all")
+  after <- imap(dbs, \(x, y) {
+    remove <- getRunsFilters(x, filter, y)
     tbl(x, "runs") |>
       anti_join(remove, by = c("runID", "motusTagID")) |>
       count(name = "n_after") |>
       collect()
   }) |>
     list_rbind()
-  
+
   bind_cols(before, after) |>
     mutate(p_reduced = (n_before - n_after) / n_before * 100)
 }
+
+gt_theme <- function(data, ...) {
+  data |>
+    tab_options(
+      table.border.top.width = px(3), 
+      column_labels.font.weight = "bold",
+      column_labels.border.bottom.width = px(3),
+      data_row.padding = px(7),
+      ...
+    )
+}
+ 
+# Collect and transform ts values to date/times 
+collect_ts <- function(db) {
+  db |>
+    collect() |>
+    mutate(across(contains("ts", ignore.case = FALSE), as_datetime))
+}
+
+# Collect run/tag information for filters
+collect_filter <- function(db) {
+  db |>
+    rename_with(~"motusTagID", .cols = any_of("tagID")) |>
+    select(runID, motusTagID) |>
+    mutate(probability = 0) |>
+    collect()
--- XX_setup.R
+++ XX_setup.R
@@ -12,6 +12,7 @@ library(tibble)
 library(stringr)
 library(ggplot2)
 library(readr)
+library(lubridate)
 library(assertr) # Check data quality
 
 # Data display
@@ -28,14 +29,33 @@ motus:::sessionVariable(name = "userLogin", val = Sys.getenv("URBAN_USER"))
 motus:::sessionVariable(name = "userPassword", val = Sys.getenv("URBAN_PASSWORD"))
 
 # ---- projects ----
-projects <- c(484, 551, 373, 168)
+projects <- c(484, 551, 373, 168, # From Barbara and Elizabeth
+              352, 364, 393, 417, 464, 515, 607) # Open Motus projects
 projects <- setNames(projects, projects)
 
-# ---- data ----
-# naturecounts::nc_metadata() # Update naturecounts taxonomy lists
+# ---- db_load ----
+dbs <- map(projects, \(x) tagme(x, dir = "Data/Raw", update = FALSE))
 
-species <- naturecounts::meta_species_taxonomy() |>
+# ---- db_species ----
+# naturecounts::nc_metadata() # Update naturecounts taxonomy lists
+species_list <- naturecounts::meta_species_taxonomy() |>
   filter(order_taxon %in% c("Passeriformes", "Piciformes"))
+
+sp <- tbl(dbs[[1]], "tagDeps") |>
+  # Only get species which are in our specific projects
+  filter(projectID %in% projects) |>
+  semi_join(x = tbl(dbs[[1]], "species"), y = _,
+            by = c("id" = "speciesID")) |>
+  select(-"group", -"sort") |>
+  distinct() |>
+  collect() |>
+  # Keep only chosen species
+  filter(id %in% species_list$species_id) |>
+  arrange(id) |>
+  # Fix subspecies names for joining with eBird (e.g., myrtle yellow-rumped warbler)
+  mutate(
+    scientific_motus = scientific,
+    scientific = str_replace(scientific, "Setophaga coronata coronata", "Setophaga coronata"))
 
 # ---- functions ----
 source("XX_functions.R")
